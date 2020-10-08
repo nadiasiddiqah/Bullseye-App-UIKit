26f2c9d96f2471a1d3980fdea1d765e23bda99ae
nadiasiddiqah <nadiasiddiqah@gmail.com>
2020-10-07 17:27:08 -0700

    Added all sliders, buttons, and labels.

--- Bullseye App (UIKit)/ViewController.swift
+++ Bullseye App (UIKit)/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var currentValue: Int = 1
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func showAlert() {
+        let message = "The value of the slider is \(currentValue)"
+        let alert = UIAlertController(title: "Hello World",
+                                      message: message,
+                                      preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func sliderMoved(slider: UISlider) {
+        currentValue = lroundf(slider.value)        // lroundf method = converts Double -> Int
+    }
 
 }
 
